4bdc6ac66f0fe341a3143808c446651fe4d28247
Pranaybairagi <109142909+Pranaybairagi@users.noreply.github.com>
2024-10-07 12:02:58 +0530

    Delete task 4 directory

--- task 4/script.js
+++
@@ -1,9 +0,0 @@
-// Task-4 Create an unorder list. Allow users to add and remove list items dynamically using buttons.
-
-var add = document.querySelector("#add");
-var remove = document.querySelector("#remove");
-var inp = document.querySelector("input");
-
-add.addEventListener("click",function(){
-
-});
